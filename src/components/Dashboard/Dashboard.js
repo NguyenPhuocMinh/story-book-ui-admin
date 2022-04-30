f1b65475f3d618136d3809d92371c894f394f89f
Minh Nguyen <xiuquay55@gmail.com>
2022-04-30 15:48:08 +0700

    modify code

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -3,6 +3,7 @@ import { Box } from '@mui/material';
 import Chart from './Chart';
 
 const DashBoard = (props) => {
+console.log("🚀 ~ file: Dashboard.js ~ line 6 ~ DashBoard ~ props", props)
   return (
     <Box>
       <h1>DashBoard</h1>
